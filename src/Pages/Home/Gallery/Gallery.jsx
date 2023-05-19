dff5e5fa20f9ac48b61d64b5489e9ee1db7ec965
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-05-19 23:23:05 +0600

    Added shop by category

--- src/Pages/Home/Gallery/Gallery.jsx
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import gauntlet from '/assets/Gallery/infinity gauntlet.jpg'
 import batman from '/assets/Gallery/batman.jpg'
-import mandalorian from '/assets/Gallery/mandalorian.jpg'
-import antman from '/assets/Gallery/antman.jpg'
-import deadpool from '/assets/Gallery/deadpool.jpg'
+import './Gallery.css'
 
 const Gallery = () => {
     return (
-        <div className=''>
+        <div className='mb-10'>
             <h2 className='text-center font-bold text-5xl my-8'>Trending collections</h2>
             <div className='flex justify-center items-center gap-5'>
                 <div className='grid grid-flow-row gap-5'>
-                    <img className='w-80' src={antman} alt="" />
-                    <img className='w-80' src={gauntlet} alt="" />
+                    <div className='h-80 w-80 text-white text-2xl text-center antman'>
+                        <h2 className='text-4xl mb-10 mt-20'>Antman</h2>
+                        <button className='btn btn-primary'>Buy now</button>
+                    </div>
+                    <div className='h-80 w-80 text-white text-2xl text-center gauntlet'>
+                        <h2 className='text-4xl mb-10 mt-20'>Gauntlet</h2>
+                        <button className='btn btn-primary'>Buy now</button>
+                    </div>
                 </div>
-                <img className='w-96' src={batman} alt="" />
+                <img className='w-96 relative' src={batman} alt="" />
+                <div className='absolute text-center text-white'>
+                    <h2 className='text-5xl mb-10'>Batman</h2>
+                    <button className='btn btn-primary'>Buy now</button>
+                </div>
                 <div className='grid grid-flow-row gap-5'>
-                    <img className='w-80' src={mandalorian} alt="" />
-                    <img className='w-80' src={deadpool} alt="" />
+                <div className='h-80 w-80 text-white text-2xl text-center mandalorian'>
+                        <h2 className='text-4xl mb-10 mt-20'>Antman</h2>
+                        <button className='btn btn-primary'>Buy now</button>
+                    </div>
+                    <div className='h-80 w-80 text-white text-2xl text-center deadpool'>
+                        <h2 className='text-4xl mb-10 mt-20'>deadpool</h2>
+                        <button className='btn btn-primary'>Buy now</button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/Pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const ShopByCategory = () => {
+    const [toys, setToys] = useState([])
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+    useEffect(() => {
+        fetchToys();
+    }, []);
+
+    const fetchToys = async () => {
+        try {
+            const response = await fetch('/toys.json');
+            const data = await response.json();
+            setToys(data);
+        } catch (error) {
+            console.error('Error fetching toys:', error);
+        }
+    };
+
+    const handleTabClick = (index) => {
+        setActiveTabIndex(index);
+    };
+
+    const renderTabList = (categories) => {
+        return (
+            <TabList className="flex mb-4">
+                {categories.map((category, index) => (
+                    <Tab
+                        key={category}
+                        className={`px-4 py-2 mr-2 cursor-pointer ${activeTabIndex === index ? 'bg-gray-200' : ''
+                            }`}
+                        onClick={() => handleTabClick(index)}
+                    >
+                        {category}
+                    </Tab>
+                ))}
+            </TabList>
+        );
+    };
+
+    const renderTabPanel = (category) => {
+        const filteredToys = toys.filter((toy) => toy.category === category);
+
+        return (
+            <TabPanel key={category}>
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {filteredToys.map((toy) => (
+                        <div key={toy.id} className="p-4 border rounded-lg flex justify-evenly items-center">
+                            <img
+                                src={toy.picture}
+                                alt={toy.name}
+                                className="mb-2 rounded-lg"
+                                style={{ maxWidth: '200px' }}
+                            />
+                            <div>
+                                <h2 className="text-2xl font-semibold text-green-400 mb-5">{toy.name}</h2>
+                                <p className="mb-1">Price: ${toy.price}</p>
+                                <p className="mb-1">Rating: {toy.rating}</p>
+                                <button className='btn btn-primary mt-5'>View Details </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </TabPanel>
+        );
+    };
+
     return (
-        <div>
-            ShopByCategory
+        <div className="container mx-auto py-8 max-w-7xl">
+            <h1 className="text-3xl mb-4 text-center">Shop by category</h1>
+            {toys.length === 0 ? (
+                <p>Loading...</p>
+            ) : (
+                <Tabs>
+                    {renderTabList(['Marvel', 'DC', 'Star Wars'])}
+                    {renderTabPanel('Marvel')}
+                    {renderTabPanel('DC')}
+                    {renderTabPanel('Star Wars')}
+                </Tabs>
+            )}
         </div>
     );
 };
